bca747c17723936cc9d50f970e72c48e97a17d97
kurwitz3 <kurwitz3@aol.com>
2021-02-03 16:24:21 -0500

    commit tree b4fa92956eba6f210df5da3b9ad9f4a87c90fa20

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers,string){
+    return drivers.filter( x => {
+        return x.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(b => {
+      return string.charAt(0) === b.charAt(0)
+    })
+}
+
+function matchName(drivers,string){
+    return drivers.filter(b => {
+      return b['name']=== string
+    })
+
+}
